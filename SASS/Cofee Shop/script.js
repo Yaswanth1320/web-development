9612d8def564c8f1dccd53910e1bb484b193e7de
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-09-26 16:07:18 +0530

    coffee_home

--- SASS/Cofee Shop/script.js
+++ SASS/Cofee Shop/script.js
@@ -1,7 +1,19 @@
 
 
-let navbar = document.querySelector('.header_navbar');
+// let header_navbar = document.querySelector('.header_navbar');
 
-document.querySelector('.btn').onclick = () =>{
-    header_navbar.classlist.toggle('active');
+// document.querySelector('#menu-btn').onclick = () =>{
+//     header_navbar.classList.toggle('active');
+// }
+
+function displayMenu(id){
+    document.getElementById("menu").style.right=0;
+    
+    
+}
+
+function displayCart(id){
+    document.getElementById("cart").style.right=0;
+    
+    
 }
