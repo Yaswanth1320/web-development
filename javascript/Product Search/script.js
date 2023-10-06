16ff23c8312444195918109cd4bcec375d9818f9
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-10-06 13:02:25 +0530

    product search

--- javascript/Product Search/script.js
+++ javascript/Product Search/script.js
@@ -1,11 +1,21 @@
 
 const search = () =>{
-    const searchInput = document.getElementById("search-input").ariaValueMax.toUpperCase();
+    const searchInput = document.getElementById("search-input").value.toUpperCase();
     const itemsList = document.getElementById("product-list");
     const product = document.querySelectorAll(".product");
-    const pname = document.getElementsByTagName("h3");
+    const pname = itemsList.getElementsByTagName("h3");
 
     for(var i=0; i <pname.length; i++){
+        let match = product[i].getElementsByTagName('h3')[0];
         
+        if(match){
+            let textValue = match.textContent || match.innerHTML;
+
+            if(textValue.toUpperCase().indexOf(searchInput) > -1){
+                product[i].style.display= "";
+            }else{
+                product[i].style.display= "none";
+            }
+        }
     }
 }
