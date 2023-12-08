feb2ad035d34f5372b87fed31caedea4a5381ece
Yaswanth1320 <yashwanth.paravathala@gmail.com>
2023-12-08 11:30:49 +0530

    landing pages

--- basic websites/starbucks/script.js
+++ basic websites/starbucks/script.js
@@ -1,15 +1,3 @@
-// let navbar = document.querySelector(".navbar");
-
-// document.querySelector("#menu-btn").onclick = () =>{
-//     navbar.classList.toggle('active');
-// }
- 
-
-const burgerMenu = document.getElementById("menu-btn");
-
-let navbar = document.querySelector(".navbar");
-
-burgerMenu.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-  console.log("working");
-});
+function imgSlider(anything) {
+  document.querySelector(".starbucks").src = anything;
+}
